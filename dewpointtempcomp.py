3d866143dcab08808b6723758e5e3fe79f116e85
Larissa Dias <lmdias@uw.edu>
2023-04-14 11:04:14 -0500

    Computed dew point temperature

---
+++ dewpointtempcomp.py
@@ -0,0 +1,18 @@
+from mysci.readdata import read_data
+from mysci.printing import print_comparison
+from mysci.computation import compute_dewpoint
+
+#Column names and column indices
+columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7, 'humout': 5, 'dewpt': 6}
+
+#Data types for each column (if non-string)
+types = {'tempout': float, 'humout': float, 'dewpt': float}
+
+#Read the data file
+data = read_data(columns, types=types)
+
+#Estimate wind chill
+dewpointtemp = [compute_dewpoint(t, h) for t, h in zip(data['tempout'], data['humout'])]
+
+# Output comparison of data
+print_comparison('DEW PT', data['date'], data['time'], data['dewpt'], dewpointtemp)
