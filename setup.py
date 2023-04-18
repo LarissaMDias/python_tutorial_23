06bacf4c116c2411adc40f202902b61751ef6d4b
Larissa Dias <lmdias@uw.edu>
2023-04-18 15:59:11 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A saample package description",
+    author="Larissa Dias",
+    author_email="lmdias@uw.edu",
+    packages=["mysci"],
+    install_requires=[])
